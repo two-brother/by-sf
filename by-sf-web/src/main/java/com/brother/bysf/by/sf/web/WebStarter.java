b41fd8fabefcf11f85119b18eff41e427ef7b54c
shifan <shifanwen@skspruce.net>
2018-02-01 09:40:22 +0800

    add jar tool

--- by-sf-web/src/main/java/com/brother/bysf/by/sf/web/WebStarter.java
+++ by-sf-web/src/main/java/com/brother/bysf/by/sf/web/WebStarter.java
@@ -1,5 +1,7 @@
 package com.brother.bysf.by.sf.web;
 
+import com.brother.bysf.by.sf.web.util.JarToolUtil;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -9,11 +11,13 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
  * @date 2017/12/14
  */
 @SpringBootApplication
+@MapperScan("com.brother.bysf.by.sf.web.db.mybatis.mapper")
 public class WebStarter {
     public static void main(String[] args) {
         new SpringApplicationBuilder()
                 .sources(WebStarter.class)
                 .bannerMode(Banner.Mode.OFF)
                 .run(args);
+        JarToolUtil.test();
     }
 }
---
+++ by-sf-web/src/main/java/com/brother/bysf/by/sf/web/util/JarToolUtil.java
@@ -0,0 +1,83 @@
+package com.brother.bysf.by.sf.web.util;
+
+import java.io.File;
+
+/**
+ * @author sk-shifanwen
+ * @date 2018/1/31
+ */
+public class JarToolUtil
+{
+    /**
+     * 获取jar绝对路径
+     *
+     * @return jar path
+     */
+    public static String getJarPath()
+    {
+        File file = getFile();
+        if (file == null) {
+            return null;
+        }
+        return file.getAbsolutePath();
+    }
+
+    /**
+     * 获取jar目录
+     *
+     * @return jar dir
+     */
+    public static String getJarDir()
+    {
+        File file = getFile();
+        if (file == null) {
+            return null;
+        }
+        return getFile().getParent();
+    }
+
+    /**
+     * 获取jar包名
+     *
+     * @return jar name
+     */
+    public static String getJarName()
+    {
+        File file = getFile();
+        if (file == null) {
+            return null;
+        }
+        return getFile().getName();
+    }
+
+    /**
+     * 获取当前Jar文件
+     *
+     * @return
+     */
+    private static File getFile()
+    {
+        // 关键是这行...
+        String path = JarToolUtil.class.getProtectionDomain().getCodeSource().getLocation().getFile();
+        try
+        {
+            // 转换处理中文及空格
+            path = java.net.URLDecoder.decode(path, "UTF-8");
+        }
+        catch (java.io.UnsupportedEncodingException e)
+        {
+            return null;
+        }
+        return new File(path);
+    }
+
+    public static void test() {
+        System.out.println("getJarPath:== " + getJarPath());
+        System.out.println("getJarDir:== " + getJarDir());
+        System.out.println("getJarName:== " + getJarName());
+    }
+
+    public static void main(String[] args) {
+        test();
+    }
+}
