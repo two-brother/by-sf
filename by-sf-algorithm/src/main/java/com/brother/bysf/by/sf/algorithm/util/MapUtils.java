efa6f367f8618f5bb374664333d357be8d28ebf2
shifan <shifanwen@skspruce.net>
2018-03-08 17:08:57 +0800

    mapUtils modify

--- by-sf-algorithm/src/main/java/com/brother/bysf/by/sf/algorithm/util/MapUtils.java
+++ by-sf-algorithm/src/main/java/com/brother/bysf/by/sf/algorithm/util/MapUtils.java
@@ -6,7 +6,34 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-public class MapUtils {
+class MapUtils {
+    static Map<String, Set<String>> mapAdd(Map<String, Set<String>> mapNew, Map<String, Set<String>> mapOld){
+        Map<String, Set<String>> mapAdd = new HashMap<>();
+        mapNew.forEach((k, v) -> mapAdd.put(k, mapOld.containsKey(k) ? v.stream().filter(s -> !mapOld.get(k).contains(s)).collect(Collectors.toSet()) : v));
+        return mapAdd.keySet().stream().filter(k -> mapAdd.get(k).size() > 0).collect(Collectors.toMap(k -> k, mapAdd::get));
+    }
+
+    static Map<String, Set<String>> mapDelete(Map<String, Set<String>> mapNew, Map<String, Set<String>> mapOld){
+        Map<String, Set<String>> mapDelete =  new HashMap<>();
+        mapOld.forEach((k, v) -> mapDelete.put(k, mapNew.containsKey(k) ? v.stream().filter(s -> !mapNew.get(k).contains(s)).collect(Collectors.toSet()) : v));
+        return mapDelete.keySet().stream().filter(k -> mapDelete.get(k).size() > 0).collect(Collectors.toMap(k -> k, mapDelete::get));
+    }
+
+    static Map<String, Set<String>> mapMerge(Map<String, Set<String>> mapNew, Map<String, Set<String>> mapOld){
+        Map<String, Set<String>> mapMerge = new HashMap<>();
+        mapNew.forEach((k, v) -> mapMerge.merge(k, v, (vo, vn) -> {
+            vo.addAll(vn);
+            return vo;
+        }));
+        mapOld.forEach((k, v) -> mapMerge.merge(k, v, (vo, vn) -> {
+            vo.addAll(vn);
+            return vo;
+        }));
+        return mapMerge;
+    }
+}
+
+class Test{
     public static void main(String[] args) {
         Map<String, Set<String>> mapNew = new HashMap<>();
         Map<String, Set<String>> mapOld = new HashMap<>();
@@ -23,20 +50,17 @@ public class MapUtils {
         mapNew.put("t2", setb);
 
         mapOld.put("t2", setc);
-        mapOld.put("t3", setc);
+        mapOld.put("t3", setb);
 
+        System.out.println("mapNew before: " + mapNew);
+        System.out.println("mapOld before: " + mapOld);
+        System.out.println("mapAdd: " + MapUtils.mapAdd(mapNew, mapOld));
+        System.out.println("mapDelete: " + MapUtils.mapDelete(mapNew, mapOld));
+        System.out.println("mapNew after: " + mapNew);
+        System.out.println("mapOld after: " + mapOld);
 
-        Map<String, Set<String>> mapCommon = mapNew.keySet().stream().filter(k -> mapOld.containsKey(k)).collect(Collectors.toMap(k -> k, k -> mapNew.get(k)));
-        System.out.println("common: " + mapCommon);
-
-
-        Map<String, Set<String>> mapAdd = mapNew.keySet().stream().map(k -> k).collect(Collectors.toMap(k -> k, k -> mapNew.get(k)));
-        Map<String, Set<String>> mapUpdate = null;
-        Map<String, Set<String>> mapDelete = null;
-
-        System.out.println("add: " + mapAdd);
-        System.out.println("update: " + mapUpdate);
-        System.out.println("delete: " + mapDelete);
-
+        System.out.println("mapMerge after: " + MapUtils.mapMerge(mapNew, mapOld));
+        System.out.println("mapNew after: " + mapNew);
+        System.out.println("mapOld after: " + mapOld);
     }
 }
